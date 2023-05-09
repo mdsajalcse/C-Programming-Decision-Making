fb1eaaef9b751de4b0df516a55a50978ce0e3eaa
ifthekar <sajalcse2018@gmail.com>
2023-05-09 12:04:25 +0600

    nested if statement

---
+++ nestedIfStatement.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+
+   int num1=10,num2=20,num3=50,num4=100,num5=200;
+
+   if(num1==10){
+       if(num2==20){
+            if(num3==50){
+
+               if(num4==100){
+                if(num5==200){
+
+                printf("Value of numer#1 is= %d\n",num1);
+                 printf("Value of numer#2 is= %d\n",num2);
+                  printf("Value of numer#3 is= %d\n",num3);
+                    printf("Value of numer#4 is= %d\n",num4);
+                      printf("Value of numer#5 is= %d\n",num5);
+                }
+               }
+            }
+    }
+   }
+
+     printf("Actual Value of numer#1 is= %d\n",num1);
+     printf("Actual Value of numer#2 is= %d\n",num2);
+     printf("Actual Value of numer#3 is= %d\n",num3);
+     printf("Actual Value of numer#4 is= %d\n",num4);
+     printf("Actual Value of numer#5 is= %d\n",num5);
+return 0;
+}
